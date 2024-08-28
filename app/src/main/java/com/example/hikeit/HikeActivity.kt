7ba26e13f485b19f9b874579dd7e54376ccd4dcb
Maciej Sieradz <macieks53@gmail.com>
2024-08-28 18:55:27 +0200

    Simple Search mock view.

--- app/src/main/java/com/example/hikeit/HikeActivity.kt
+++ app/src/main/java/com/example/hikeit/HikeActivity.kt
@@ -14,12 +14,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
